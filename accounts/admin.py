82117004ba6c9cc065aa1976c604e8a3e5ab849e
swhan9404 <swhan9404@naver.com>
2021-05-27 09:29:47 +0900

    admin 페이지에 user 추가

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from django.contrib.auth import get_user_model
 
 # Register your models here.
+class UserAdmin(admin.ModelAdmin) :
+    list_display = ("id", "username", "email")
+
+admin.site.register(get_user_model(), UserAdmin)
